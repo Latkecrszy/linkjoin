cd7b38303f8405c805de4a6e14573d922f4f8b0a
Seth Raphael <setharaphael7@gmail.com>
2021-02-22 10:27:46 -0800

    implemented time in javascript

--- static/redirect.js
+++ static/redirect.js
@@ -29,29 +29,14 @@ function sleep(ms) {
   }
 
 async function NewTab(username) {
-    let date = new Date()
-    day = {0: "Mon", 1: "Tue", 2: "Wed", 3: "Thu", 4: "Fri", 5: "Sat", 6: "Sun"}[parseInt(date.getDay())]
-    hour = parseInt(date.getHours())
-    minute = parseInt(date.getMinutes())
-    let start_json = await fetch(`https://linkjoin.xyz/db?username=${username}`)
-    let user_links = await start_json.json()
-    console.log(user_links)
-    let link_hour;
-    let link_minute;
-    for (const link of user_links) {
-        link_hour = parseInt(link["time"].split(":")[0])
-        link_minute = parseInt(link["time"].split(":")[1])
-        console.log(`days: ${link["days"]}, link_hour: ${link_hour}, link_minute: ${link_minute}`)
-        if (link["days"].includes(day) && hour == link_hour && minute == link_minute && link["active"] == "true") {
-            window.open(link["link"])
-        }
-    }
-    console.log(`day: ${day}, hour: ${hour}, minute: ${minute}`)
     while (true) {
-        date = new Date()
-        day = {0: "Mon", 1: "Tue", 2: "Wed", 3: "Thu", 4: "Fri", 5: "Sat", 6: "Sun"}[parseInt(date.getDay())]
-        start_json = await fetch(`https://linkjoin.xyz/db?username=${username}`)
-        user_links = await start_json.json()
+        let date = new Date()
+        console.log(date.getDay())
+        let day = {1: "Mon", 2: "Tue", 3: "Wed", 4: "Thu", 5: "Fri", 6: "Sat", 7: "Sun"}[parseInt(date.getDay())]
+        let start_json = await fetch(`https://linkjoin.xyz/db?username=${username}`)
+        let user_links = await start_json.json()
+        let link_hour;
+        let link_minute;
         console.log(user_links)
         for (const link of user_links) {
             link_hour = parseInt(link["time"].split(":")[0])
@@ -63,7 +48,7 @@ async function NewTab(username) {
             }
         }
         console.log(`day: ${day}, hour: ${parseInt(date.getHours())}, minute: ${parseInt(date.getMinutes())}`)
-        sleep(15000)
+        await sleep(15000)
     }
 }
 
