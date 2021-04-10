af48c118d924e255b236973d9df57c15247eabed
Latkecrszy <setharaphael@gmail.com>
2021-04-09 21:33:07 -0700

    testing

--- static/links.js
+++ static/links.js
@@ -310,6 +310,7 @@ async function load_links(username, sort) {
         }
     }
     let tutorial_completed = await fetch(`https://linkjoin.xyz/tutorial_complete?username=${username}`)
+    console.log(tutorial_completed)
     tutorial_completed = await tutorial_completed.json()
     console.log(tutorial_completed)
     if (tutorial_completed['tutorial'] === "false") {await tutorial(0)}
