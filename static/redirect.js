9cc4658a2e81be6fdda430f27f323140ea935095
Latkecrszy <setharaphael@gmail.com>
2021-05-04 23:25:14 -0700

    testing

--- static/redirect.js
+++ static/redirect.js
@@ -47,7 +47,7 @@ async function NewTab(username, links, sort) {
             }
         }
         if (JSON.stringify(user_links) !== JSON.stringify(links)) { console.log("refresh")
-        load_links(username, sort); return}
+        load_links(username, sort); restart = true; continue}
         // MAKE PAGE REFRESH WHEN CONTENT CHANGES
         await sleep(15000)
 
