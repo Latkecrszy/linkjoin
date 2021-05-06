62ba6e3c8e8ea2fb5c262b2e12bccdbed4b50c6d
Latkecrszy <setharaphael@gmail.com>
2021-05-06 15:24:05 -0700

    refreshed mobile header

--- static/links.js
+++ static/links.js
@@ -483,3 +483,5 @@ async function refresh() {
     let insert = document.getElementById("insert")
     while (insert.firstChild) {insert.removeChild(insert.firstChild)}
 }
+
+document.addEventListener("click", x => {if (x.target.matches("#hamburger") || x.target.matches("#line1") || x.target.matches("#line2") || x.target.matches("#line3")) {document.getElementById("hamburger").classList.toggle("expand"); document.getElementById("hamburger_dropdown").classList.toggle("expand")}})
