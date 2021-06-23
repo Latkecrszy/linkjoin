82f820d5b893d6910cc2d9e19ece81df68aee36b
Seth Raphael <setharaphael7@gmail.com>
2021-06-23 14:07:28 -0700

    update to location endpoint

--- app.py
+++ app.py
@@ -432,7 +432,7 @@ def receive_vonage_message():
 @app.route("/location")
 def location():
     x = requests.get(f'http://ip-api.com/json/{request.remote_addr}')
-    return x.json()
+    return x.json(), x.headers
 
 
 app.register_error_handler(404, lambda e: render_template('404.html'))
