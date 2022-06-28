724484d1c8d12cb59c229205b76ff5a31cd37c8f
Seth Raphael <setharaphael7@gmail.com>
2022-06-27 19:37:40 -0700

    Use secure wss

--- static/links.js
+++ static/links.js
@@ -384,7 +384,7 @@ async function load_links(username, sort, id="insert") {
         return
     }
     global_username = username
-    webSocket = new WebSocket(`ws://linkjoin-beta.herokuapp.com/database_ws?email=${encodeURIComponent(username)}`)
+    webSocket = new WebSocket(`wss://linkjoin-beta.herokuapp.com/database_ws?email=${encodeURIComponent(username)}`)
     webSocket.onopen = () => {
         console.log('working')
         webSocket.send(JSON.stringify({'email': username}))
